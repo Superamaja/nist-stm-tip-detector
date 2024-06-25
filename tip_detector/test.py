5a3a0ff75f9f50bdcc21bc7372d4667055cd8be2
Connor Lin <connorlin06@gmail.com>
2024-06-25 15:19:07 -0400

    Updated to use refactored functions

--- tip_detector/test.py
+++ tip_detector/test.py
@@ -4,7 +4,9 @@ import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
 
-image_path = "full_scan_examples/sharp_single.png"
+from helpers import preprocess_image
+
+image_path = "full_scan_examples/example_0.png"
 
 # Images 1, 2 use (6,6) with 0.01
 CONTOUR_MIN_SIZE = (6, 6)  # Minimum size of the contour to pass (width, height)
@@ -25,38 +27,14 @@ if len(sys.argv) > 1:
 
 # Load the pre-trained model
 print("loading model")
-model = load_model("stm_model.h5")
-# model = load_model("model.h5")
-
-
-# Define a function to preprocess the image for the model
-def preprocess_image(roi):
-    roi = cv2.resize(roi, (75, 75), interpolation=cv2.INTER_AREA)
-    roi = roi.astype("float32") / 255.0
-    roi = np.expand_dims(roi, axis=-1)
-    roi = np.expand_dims(roi, axis=0)
-    return roi
-
+# model = load_model("stm_model.h5")
+model = load_model("model.h5")
 
 # Load the image file
 img = cv2.imread(image_path)
 img2 = img.copy()
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-
-def process_image(img, alpha):
-    global img_contrast, gray_contrast, blurred_contrast, edged_contrast, contours
-
-    # Lowering contrast avoids impurities and prevents the need of size thresholding
-    img_contrast = cv2.convertScaleAbs(img, alpha=alpha, beta=0)
-    gray_contrast = cv2.cvtColor(img_contrast, cv2.COLOR_BGR2GRAY)
-    blurred_contrast = cv2.GaussianBlur(gray_contrast, (5, 5), 0)
-    edged_contrast = cv2.Canny(blurred_contrast, 50, 150)
-    contours, _ = cv2.findContours(
-        edged_contrast, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-    )
-
 
 # Process the image with different contrast levels
 
