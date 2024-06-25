Attempting to fix via normalization

tip_detector/helpers.py
@@ -6,8 +6,9 @@ def preprocess_image(roi):
     """
     Preprocess the image before feeding it to the model.
     """
-    if roi.shape[0] != 50 or roi.shape[1] != 50:
-        roi = cv2.resize(roi, (50, 50), interpolation=cv2.INTER_AREA)
+    if roi.shape[0] != 75 or roi.shape[1] != 75:
+        roi = cv2.resize(roi, (75, 75), interpolation=cv2.INTER_LINEAR)
+    roi = cv2.normalize(roi, None, 0, 255, cv2.NORM_MINMAX)
     roi = roi.astype("float32") / 255.0
     roi = np.expand_dims(roi, axis=-1)
     roi = np.expand_dims(roi, axis=0)

tip_detector/tip_detector.py
@@ -4,13 +4,14 @@ import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
 
-from helpers import extract_roi, find_contours, preprocess_image
+from helpers import extract_roi, find_contours, preprocess_image, resize_roi
 
-image_path = "full_scan_examples/full_scan_example2.png"
+image_path = "full_scan_examples/full_scan_example1.png"
 
 # Images 1, 2 use (6,6) with 0.01
 CONTOUR_MIN_SIZE = (6, 6)  # Minimum size of the contour to pass (width, height)
-SHARP_PREDICTION_THRESHOLD = 0.5  # Prediction threshold for sharpness - Greater than or equal to this value is sharp, otherwise blunt
+SHARP_PREDICTION_THRESHOLD = 0.1  # Prediction threshold for sharpness - Greater than or equal to this value is sharp, otherwise dull
+DEBUG = True
 
 RED = (50, 50, 255)
 GREEN = (0, 255, 0)
@@ -43,14 +44,19 @@ if len(contours) == 0:
 
 total_bonds = 0
 total_cls = {0: 0, 1: 0}
+
+nm_p_pixel = 45 / img.shape[1]
+# 1: 45nm x 45nm
+# 2:
+
 for cnt in contours:
     x, y, w, h = cv2.boundingRect(cnt)
     if w >= CONTOUR_MIN_SIZE[0] and h >= CONTOUR_MIN_SIZE[1]:
-        roi, x, y, square_size = extract_roi(gray, x, y, w, h)
+        roi, x, y, square_size = extract_roi(gray, x, y, x + w, y + h)
+        roi, x, y, new_size = resize_roi(gray, x, y, square_size, int(2 / nm_p_pixel))
         if roi.shape[0] == 0 or roi.shape[1] == 0:
             continue
         roi_preprocessed = preprocess_image(roi)
-        print(roi.shape, roi_preprocessed.shape)
         prediction = model.predict(roi_preprocessed)[0][0]
         # cls = np.argmax(prediction)
         cls = 1 if prediction >= SHARP_PREDICTION_THRESHOLD else 0
@@ -60,7 +66,7 @@ for cnt in contours:
         cv2.rectangle(
             img,
             (x, y),
-            (x + square_size, y + square_size),
+            (x + new_size, y + new_size),
             GREEN if cls else RED,
             0,
         )
@@ -84,6 +90,12 @@ for cnt in contours:
         # Count the number of contours
         total_bonds += 1
         total_cls[cls] += 1
+
+        if DEBUG:
+            cv2.imshow("ROI2", roi_preprocessed[0])
+            cv2.moveWindow("ROI2", 75, 400)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
 decision = 0 if total_cls[0] > total_cls[1] else 1
 percent = total_cls[decision] / total_bonds * 100