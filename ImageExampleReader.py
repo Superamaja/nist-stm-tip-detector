a3ed4617f37755d2ca81547dad8e7e17bbffc79e
Connor Lin <connorlin06@gmail.com>
2024-06-27 15:25:17 -0400

    Converted and modernized data augmentation to 90 deg rotations + flips

--- ImageExampleReader.py
+++ ImageExampleReader.py
@@ -1,22 +1,16 @@
 import json
 import os
 import random
-import time
 from datetime import datetime
-from math import ceil
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 from tensorflow.keras import Sequential, layers  # type: ignore
 from tensorflow.keras.callbacks import EarlyStopping  # type: ignore
 from tensorflow.keras.optimizers import Adam  # type: ignore
-from tensorflow.keras.preprocessing.image import (  # type: ignore
-    ImageDataGenerator,
-    apply_affine_transform,
-    img_to_array,
-    load_img,
-)
+from tensorflow.keras.preprocessing.image import img_to_array, load_img  # type: ignore
 
 from helpers import get_ordered_fnames
 
@@ -24,6 +18,7 @@ example_dirs = ["processed_data"]
 vsplit = 0.2
 batch_size = 16
 xforms_per_image = 1
+enable_augmentation = True
 folder_extension = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 
 
@@ -81,21 +76,16 @@ for example_dir in example_dirs:
 img_data = []
 
 for fname in overall_fnames:
-    img = load_img(fname, target_size=(SQUARE_PIXEL_SIZE, SQUARE_PIXEL_SIZE), color_mode="grayscale")
+    img = load_img(
+        fname,
+        target_size=(SQUARE_PIXEL_SIZE, SQUARE_PIXEL_SIZE),
+        color_mode="grayscale",
+    )
     img_data.append(img_to_array(img))  # numpy array
 
 
 img_data = np.array(img_data)
 print(f"data shape: {img_data.shape}")
-
-max = 0
-for row in img_data[0]:
-    for px in row:
-        for val in px:
-            if val > max:
-                max = val
-print("max: " + str(max))
-
 
 # read in the features/labels which are ordered the same as img_data
 features = pd.read_csv(os.path.join(example_dirs[0], "features.csv"), sep=",")
@@ -121,11 +111,52 @@ sharp_indexes = db_sharp_indexes.tolist()
 
 # --------------------------------------------------------------------------------------------------------
 # "standard" ML processing starts here
+
+
+# Function to apply the required augmentations
+def augment_image(image):
+    # Convert image to float32 and scale to [0, 1]
+    image = tf.image.convert_image_dtype(image, tf.float32)
+
+    # Generate 8 augmented images: 4 rotations and 4 flips
+    # TODO: Zoom and brightness
+    augmented_images = []
+    for i in range(4):
+        rotated_image = tf.image.rot90(image, k=i)
+        augmented_images.append(rotated_image)
+        augmented_images.append(tf.image.flip_left_right(rotated_image))
+
+    return augmented_images if enable_augmentation else [image]
+
+
+# Convert numpy arrays to tf.data.Dataset
+def create_dataset(images, labels, batch_size, training=False):
+    dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+
+    if training:
+        dataset = dataset.shuffle(buffer_size=len(images))
+
+    dataset = dataset.map(
+        lambda x, y: (tf.image.convert_image_dtype(x, tf.float32), y),
+        num_parallel_calls=tf.data.experimental.AUTOTUNE,
+    )
+
+    if training and enable_augmentation:
+        dataset = dataset.flat_map(
+            lambda x, y: tf.data.Dataset.from_tensor_slices((augment_image(x), [y] * 8))
+        )
+
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.repeat()
+    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+    return dataset
+
+
 random.shuffle(dull_indexes)
 random.shuffle(sharp_indexes)
 
-
-# validation split
+# Validation split
 dull_indexes_v, dull_indexes_t = validation_split_list(
     dull_indexes, validation_split=vsplit
 )
@@ -136,50 +167,12 @@ sharp_indexes_v, sharp_indexes_t = validation_split_list(
 indexes_v = dull_indexes_v + sharp_indexes_v
 indexes_t = dull_indexes_t + sharp_indexes_t
 
-
-# create the final validation and test arrays
-validation_images = []
-for i in indexes_v:
-    validation_images.append(img_data[i])
-
-validation_images = np.array(validation_images)
-
-# training images can be manually augmented here
-train_images = []
-for i in indexes_t:
-    # 8 transforms per image (xforms_per_image):
-    # 4x 90 degree rotations (incl. 0 deg), and horizontal flips of each
-
-    img = img_data[i]
-    train_images.append(img)
-
-    if xforms_per_image == 8:
-        flip_img = apply_affine_transform(img, zx=-1)
-        train_images.append(flip_img)
-
-        img = apply_affine_transform(img, theta=90)
-        train_images.append(img)
-        flip_img = apply_affine_transform(img, zx=-1)
-        train_images.append(flip_img)
-
-        img = apply_affine_transform(img, theta=90)
-        train_images.append(img)
-        flip_img = apply_affine_transform(img, zx=-1)
-        train_images.append(flip_img)
-
-        img = apply_affine_transform(img, theta=90)
-        train_images.append(img)
-        flip_img = apply_affine_transform(img, zx=-1)
-        train_images.append(flip_img)
-
-train_images = np.array(train_images)
-
+# Create the final validation and test arrays
+validation_images = np.array([img_data[i] for i in indexes_v])
+train_images = np.array([img_data[i] for i in indexes_t])
 
 validation_labels = np.array([0] * len(dull_indexes_v) + [1] * len(sharp_indexes_v))
-train_labels = np.array(
-    [0] * len(dull_indexes_t) * xforms_per_image
-    + [1] * len(sharp_indexes_t) * xforms_per_image
-)
+train_labels = np.array([0] * len(dull_indexes_t) + [1] * len(sharp_indexes_t))
 
 print("validation labels: " + str(validation_labels.shape))
 print("validation images: " + str(validation_images.shape))
@@ -187,43 +180,14 @@ print("validation images: " + str(validation_images.shape))
 print("train labels: " + str(train_labels.shape))
 print("train images: " + str(train_images.shape))
 
-max = 0
-for row in train_images[0]:
-    for px in row:
-        for c in px:
-            if c > max:
-                max = c
 
-print("max: " + str(max))
-
-if xforms_per_image == 1:
-    train_datagen = ImageDataGenerator(
-        rescale=1.0 / 255,
-        rotation_range=360,
-        # width_shift_range = 0.1,
-        # height_shift_range = 0.1,
-        shear_range=0.15,
-        zoom_range=0.05,
-        horizontal_flip=True,
-    )
-else:
-    train_datagen = ImageDataGenerator(
-        rescale=1.0 / 255, shear_range=0.1, zoom_range=0.05
-    )
-
-
-val_datagen = ImageDataGenerator(rescale=1.0 / 255)
-
-train_generator = train_datagen.flow(
-    train_images, y=train_labels, batch_size=batch_size
+# Create tf.data.Dataset instances
+train_dataset = create_dataset(train_images, train_labels, batch_size, training=True)
+validation_dataset = create_dataset(
+    validation_images, validation_labels, batch_size, training=False
 )
 
-validation_generator = val_datagen.flow(
-    validation_images, y=validation_labels, batch_size=batch_size
-)
-
-
-# build the convnet
+# Build the convnet
 model = Sequential(
     [
         layers.Input(shape=(SQUARE_PIXEL_SIZE, SQUARE_PIXEL_SIZE, 1)),
@@ -235,7 +199,6 @@ model = Sequential(
         layers.Dense(1, activation="sigmoid"),
     ]
 )
-
 
 model.compile(
     loss="binary_crossentropy",
@@ -250,15 +213,18 @@ early_stopping = EarlyStopping(
     min_delta=0.0001,
 )
 
-
 history = model.fit(
-    train_generator,
-    validation_data=validation_generator,
+    train_dataset,
+    validation_data=validation_dataset,
     epochs=500,
     verbose=1,
     callbacks=[early_stopping],
-    # steps_per_epoch=ceil(len(train_labels) / batch_size),
-    # validation_steps=ceil(len(validation_labels) / batch_size),
+    steps_per_epoch=(
+        (len(train_labels) * 8 // batch_size)
+        if enable_augmentation
+        else len(train_labels) // batch_size
+    ),
+    validation_steps=len(validation_labels) // batch_size,
 )
 
 
@@ -288,12 +254,13 @@ with open(f"{folder_directory}/logs.txt", "w") as f:
 Square NM Size: {SQUARE_NM_SIZE}
 Pixel Size: {SQUARE_PIXEL_SIZE}
 Augmentation Setting: {xforms_per_image}
+Batch Size: {batch_size}
         
 --Training Info--
 Epochs: {len(history.history["acc"])}
 
-Steps per Epoch: {ceil(len(train_labels)/batch_size)}
-Validation Steps: {ceil(len(validation_labels)/batch_size)}
+Steps per Epoch: {(len(train_labels) * 8 // batch_size) if enable_augmentation else len(train_labels) // batch_size}
+Validation Steps: {len(validation_labels) // batch_size}
 
 --Model Info--
 Model Best/Restored Validation Loss: {history.history["val_loss"][np.argmin(history.history["val_loss"])]}
